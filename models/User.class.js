d0bbe4b56f2149f7c506f655bd6d932a747581ff
vi-lcandido <vitoria.lcandido@gmail.com>
2022-11-14 16:24:04 -0300

    criando classe usuário

--- models/User.class.js
+++ models/User.class.js
@@ -1,5 +1,50 @@
+const Functions = require("./functions");
 class User {
+  #idUser;
+  #name;
+  #password;
+  #email;
+  #friends;
+  #posts;
+
+  constructor(name, password, email) {
+    this.#idUser = Functions.createRandomId();
+    this.#name = name;
+    this.#password = password;
+    this.#email = email;
+    this.#friends = [];
+    this.#posts = [];
+  }
+
+  get idUser() {
+    return this.#idUser;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  set name(newName) {
+    this.#name = newName;
+  }
+
+  get password() {
+    return this.#password;
+  }
+
+  set password(newPassword) {
+    this.#password = newPassword;
+  }
+
+  get email() {
+    return this.#email;
+  }
+
+  set email(newEmail) {
+    return (this.#email = newEmail);
+  }
 
 }
 
 module.exports = User;
+
