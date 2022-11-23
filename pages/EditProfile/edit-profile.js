17d981f6152579681d6dd9e42c0fb7562de565ad
ivina albuquerque <ivina.albuquerque.s@gmail.com>
2022-11-23 14:42:00 -0300

    estruturação inputs edit-profile

---
+++ pages/EditProfile/edit-profile.js
@@ -0,0 +1,8 @@
+import User from "../../models/User.class.mjs";
+import Functions from "../../models/Functions.class.mjs";
+import Alert from "../../components/Alert.js";
+import database from "../../models/DataBase.class.mjs";
+
+console.log(database.users);
+/*Elementos*/
+const editName = (document.getElementById("userName-edit").value = `oi`);
