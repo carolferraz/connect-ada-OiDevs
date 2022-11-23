3459806e2f153ed266b0c02b82aca25e1d0c23bf
Amanda Guerra <guerraamanda1@hotmail.com>
2022-11-22 21:09:27 -0300

    validação login

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-import Functions from "./models/Functions.class.mjs";
-import Alert from "./components/Alert.js";
+import Functions from './models/Functions.class.mjs';
+import Alert from './components/Alert.js';
 
 //Alert
 
-const userNotFoundAlert = new Alert('Email e/ou senha incorreta', '../../assets/warning.svg', '#F75A68');
+const userNotFoundAlert = new Alert(
+  'Email e/ou senha incorreta',
+  '../../assets/warning.svg',
+  '#F75A68'
+);
 
 const alertCloseBtn = document.getElementById('alert-close-btn');
 
@@ -11,12 +15,26 @@ alertCloseBtn.addEventListener('click', () => userNotFoundAlert.hideAlert());
 
 //fim do alert
 
-const loginBtn = document.getElementById('loginBtn')
+const loginBtn = document.getElementById('loginBtn');
+const userEmail = document.getElementById('userEmail');
+const userPassword = document.getElementById('userPassword');
+const users = Functions.getLocalStorage('users')
 
-function startSession(e) {
-  e.preventDefault()
-  userNotFoundAlert.showAlert()
+function isUserCorrect(email, password) {
+  for (let index = 0; index < users.length; index++) {
+    if(users[index].email === email && users[index].password === password) {
+      return true
+    }
+  }
 }
 
-loginBtn.addEventListener('click', startSession)
+function startSession(e) {
+  e.preventDefault();
+  if(isUserCorrect(userEmail.value, userPassword.value)) {
+    console.log('usuário logado')
+  } else {
+    userNotFoundAlert.showAlert();
+  }
+}
 
+loginBtn.addEventListener('click', startSession);
