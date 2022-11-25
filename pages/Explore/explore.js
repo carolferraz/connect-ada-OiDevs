8e33cdff1917c95bd9e2e092f4281ce4e1580b42
Amanda Guerra <guerraamanda1@hotmail.com>
2022-11-25 16:13:35 -0300

    organizando os links do header

--- pages/Explore/explore.js
+++ pages/Explore/explore.js
@@ -5,12 +5,12 @@ import Header from "../../components/Header.js"
 //renderizando header
 const header = new Header()
 header.addMenuLink('../../assets/home.svg', "./feed.html")
-header.addMenuLink('../../assets/search.svg', "./explorer.html", true)
+header.addMenuLink('../../assets/search.svg', "../Explore/explore.html", true)
 header.addMenuLink('../../assets/new.svg', "./new.html")
 header.addProfileDropdownLink('Ver perfil', "./profile.html")
 header.addProfileDropdownLink('Editar Perfil', "./edit-profile.html")
 header.addProfileDropdownLink('Seguindo', "./following.html")
-header.addProfileDropdownLink('Sair', "./login.html", false, true)
+header.addProfileDropdownLink('Sair', "../../index.html", false, true)
 header.renderMenuLinks()
 header.renderDropDownMenu('../../assets/woman.jpg') 
 // Fim da header
