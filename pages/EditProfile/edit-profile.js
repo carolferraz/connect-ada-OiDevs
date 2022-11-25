fe6be31763a66f02695e79b29307c1d5acede01c
ivina albuquerque <ivina.albuquerque.s@gmail.com>
2022-11-25 03:16:32 -0300

    empurrando a edição do currentUser para o localstorage

--- pages/EditProfile/edit-profile.js
+++ pages/EditProfile/edit-profile.js
@@ -23,7 +23,66 @@ menuDropDown.addEventListener("click", () => {
   dropDownContent.classList.toggle("show");
 });
 
-/*Elementos*/
-const editName = (document.getElementById(
+database.initialization();
+
+/*renderizando os values do currentUserInSession*/
+// os valores default não estão sumindo quando clico nos inputs. irei modificar depois
+//daqui até o currentemail tem que ficar dentro do evendo do botão de editar perfil
+const currentName = (document.getElementById(
   "userName-edit"
 ).value = `${database.currentUserInSession.name}`);
+
+const currentImage = (document.getElementById(
+  "img-edit"
+).value = `${database.currentUserInSession.image}`);
+
+const currentRole = (document.getElementById("role-edit").value = `${
+  database.currentUserInSession.role || ""
+}`);
+
+const currentEmail = (document.getElementById(
+  "userEmail-edit"
+).value = `${database.currentUserInSession.email}`);
+
+/*Seleção Ids dos inputs*/
+
+const btnSaveEdit = document.getElementById("save-edit-profile");
+
+const newImg = document.getElementById("img-edit");
+
+const newName = document.getElementById("userName-edit");
+
+const newRole = document.getElementById("role-edit");
+
+const newEmail = document.getElementById("userEmail-edit");
+
+const currentPass = document.getElementById("old-password");
+
+const newPass = document.getElementById("userPassword-edit");
+
+const confirmNewPass = document.getElementById("userPassword-2-edit");
+
+/*Eventos em botões*/
+
+btnSaveEdit.addEventListener("click", function (e) {
+  e.preventDefault();
+  //tenho que chamar aqui dentro a função de validação de dados
+  //tenho que chamar aqui dentro a função de empurar o perfil editado para o database
+  changeDataCurrentUser(database.currentUserInSession);
+});
+
+/*Funções*/
+
+function changeDataCurrentUser(currentUserInSession) {
+  const index = database.users.findIndex(
+    (element) => element.id === currentUserInSession.id
+  );
+
+  database.users[index].image = newImg.value;
+  database.users[index].name = newName.value;
+  database.users[index].role = newRole.value;
+  database.users[index].email = newEmail.value;
+  database.users[index].password = newPass.value;
+
+  Functions.setLocalStorage("currentUserInSession", database.users[index]);
+}
