fc985cb0bbd8d8ddb1b02fe3c4de706a65988f99
Junior <jcarjrctba@hotmail.com>
2022-11-27 00:08:39 -0300

    Botão dropdown melhorado

--- pages/Following/script-following.js
+++ pages/Following/script-following.js
@@ -1,18 +1,6 @@
 import Header from '../../components/Header.js';
 import database from '../../models/DataBase.class.mjs';
 
-//renderizando header
-const header = new Header();
-header.addMenuLink('../../assets/home.svg', './feed.html');
-header.addMenuLink('../../assets/search.svg', './explorer.html');
-header.addMenuLink('../../assets/new.svg', './new.html');
-header.addProfileDropdownLink('Ver perfil', './profile.html');
-header.addProfileDropdownLink('Editar Perfil', './edit-profile.html');
-header.addProfileDropdownLink('Seguindo', './following.html', true);
-header.addProfileDropdownLink('Sair', './login.html', false, true);
-header.renderMenuLinks();
-header.renderDropDownMenu('../../assets/woman.jpg');
-
 const currentImg = `${database.currentUserInSession.image}`;
 
 //renderizando header
