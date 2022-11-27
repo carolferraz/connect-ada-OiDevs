f20f9a61d576251a3b597a4faa1ab0b0f0dbae1f
Junior <jcarjrctba@hotmail.com>
2022-11-27 17:54:03 -0300

    Style do profile card, fix linha 74

--- pages/NewPost/new-post-script.js
+++ pages/NewPost/new-post-script.js
@@ -4,6 +4,8 @@ import Functions from '../../models/Functions.class.mjs';
 import Post from '../../models/Post.class.mjs';
 import Alert from '../../components/Alert.js';
 
+const currentImg = `${database.currentUserInSession.image}`;
+
 //renderizando header
 const header = new Header();
 header.addMenuLink('../../assets/home.svg', '../Feed/feed.html');
@@ -14,13 +16,10 @@ header.addProfileDropdownLink(
   'Editar Perfil',
   '../EditProfile/edit-profile.html'
 );
-header.addProfileDropdownLink(
-  'Seguindo',
-  '../Following/following.html'
-);
+header.addProfileDropdownLink('Seguindo', '../Following/following.html');
 header.addProfileDropdownLink('Sair', '../../index.html', false, true);
 header.renderMenuLinks();
-header.renderDropDownMenu('../../assets/woman.jpg');
+header.renderDropDownMenu(currentImg);
 // Fim da header
 
 const successPublishedPostAlert = new Alert(
@@ -34,9 +33,9 @@ const functions = new Functions();
 //inicializando database
 database.initialization();
 
-const inputs = document.querySelectorAll('.required')
-const invalidMsg = document.querySelectorAll('.invalid-msg')
-const cancelBtn = document.getElementById('cancel')
+const inputs = document.querySelectorAll('.required');
+const invalidMsg = document.querySelectorAll('.invalid-msg');
+const cancelBtn = document.getElementById('cancel');
 const publishPostBtn = document.getElementById('publish');
 const postTitle = document.getElementById('post-title');
 const postDescription = document.getElementById('post-description');
@@ -44,23 +43,23 @@ const postDescription = document.getElementById('post-description');
 inputs.forEach((input, index) => {
   input.addEventListener('click', () => {
     input.style.border = '1px solid #8D8D99';
-    invalidMsg[index].style.display = 'none'
-  })
-})
+    invalidMsg[index].style.display = 'none';
+  });
+});
 
 function validation() {
   for (let index = 0; index < inputs.length; index++) {
-    if(inputs[index].value === '') {
-      functions.errorInvalidInput(index)
-      return false
+    if (inputs[index].value === '') {
+      functions.errorInvalidInput(index);
+      return false;
     }
   }
-  return true
+  return true;
 }
 
 function publishPost(e) {
-  e.preventDefault()
-  if(validation()) {
+  e.preventDefault();
+  if (validation()) {
     new Post(
       database.currentUserInSession.id,
       postTitle.value,
@@ -73,10 +72,10 @@ function publishPost(e) {
 }
 
 function backToOtherPage(e) {
-  e.preventDefault()
-  window.history.back()
+  e.preventDefault();
+  window.history.back();
 }
 
 publishPostBtn.addEventListener('click', publishPost);
 
-cancelBtn.addEventListener('click', backToOtherPage)
+cancelBtn.addEventListener('click', backToOtherPage);
