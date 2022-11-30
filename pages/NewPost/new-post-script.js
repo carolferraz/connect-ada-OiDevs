aefc2a6a24c03b25501bb4b4bf6ab234175afd0e
Amanda Guerra <guerraamanda1@hotmail.com>
2022-11-30 16:55:11 -0300

    função apagar usuário

--- pages/NewPost/new-post-script.js
+++ pages/NewPost/new-post-script.js
@@ -7,7 +7,7 @@ import Alert from "../../components/Alert.js";
 const currentImg = `${database.currentUserInSession.image}`;
 //renderizando header
 const header = new Header();
-header.addMenuLink("../../assets/home.svg", "../Feed/feed.html", true);
+header.addMenuLink("../../assets/home.svg", "../Feed/feed.html");
 header.addMenuLink("../../assets/search.svg", "../Explore/explore.html");
 header.addMenuLink("../../assets/new.svg", "../NewPost/new-post.html", true);
 header.addProfileDropdownLink("Ver perfil", "../Profile/profile.html");
