b8a115f017cf56314a996d45212c0dd1b4c71d5a
ivina albuquerque <ivina.albuquerque.s@gmail.com>
2022-12-01 15:57:08 -0300

    implementado o novo atributo na classe manager

--- main.js
+++ main.js
@@ -14,53 +14,59 @@ const userNotFoundAlert = new Alert(
 //fim do alert
 
 database.initialization();
-createAdm()
+createAdm();
 
 const loginBtn = document.getElementById("loginBtn");
 const userEmail = document.getElementById("userEmail");
 const userPassword = document.getElementById("userPassword");
-const isAdmInput = document.getElementById('is-adm')
+const isAdmInput = document.getElementById("is-adm");
 
 function createAdm() {
-  new Manager('Manager', '123456', 'manager@connectada.com')
+  new Manager("Manager", "123456", "manager@connectada.com");
 }
 
 function userExists(email, password) {
   for (let i = 0; i < database.users.length; i++) {
-    if (email === database.users[i].email && password === database.users[i].password) {
-      Functions.setLocalStorage('currentUserInSession', database.users[i]);
-      return true
-    } 
+    if (
+      email === database.users[i].email &&
+      password === database.users[i].password
+    ) {
+      Functions.setLocalStorage("currentUserInSession", database.users[i]);
+      return true;
+    }
   }
 }
 
 function authenticateUser() {
-    if (userExists(userEmail.value, userPassword.value)) {
-      return window.location.href = "./pages/Feed/feed.html";
-    } else {
-      return userNotFoundAlert.showAlert();
-    }
-  } 
-
-function authenticateAdm(email, password) {
-     if (email === database.manager.email && password === database.manager.password) {
-      Functions.setLocalStorage('currentUserInSession', database.manager)
-      window.location.href = "./pages/AdmFeed/adm-feed.html"
-    } else {
-      userNotFoundAlert.showAlert();
-    }
+  if (userExists(userEmail.value, userPassword.value)) {
+    return (window.location.href = "./pages/Feed/feed.html");
+  } else {
+    return userNotFoundAlert.showAlert();
+  }
 }
 
+function authenticateAdm(email, password) {
+  if (
+    email === database.manager.email &&
+    password === database.manager.password
+  ) {
+    Functions.setLocalStorage("currentUserInSession", database.manager);
+    window.location.href = "./pages/AdmFeed/adm-feed.html";
+  } else {
+    userNotFoundAlert.showAlert();
+  }
+}
 
 function startSession(e) {
   e.preventDefault();
-  if(isAdmInput.checked) {
-    authenticateAdm(userEmail.value, userPassword.value)
+  if (isAdmInput.checked) {
+    authenticateAdm(userEmail.value, userPassword.value);
   } else {
-    authenticateUser()
+    authenticateUser();
   }
 }
 
 loginBtn.addEventListener("click", startSession);
-
+console.log(database.users);
+console.log(database.manager);
 export default loginBtn;
--- models/Manager.class.mjs
+++ models/Manager.class.mjs
@@ -1,18 +1,26 @@
-import User from './User.class.mjs';
-import database from './DataBase.class.mjs';
+import User from "./User.class.mjs";
+import database from "./DataBase.class.mjs";
 
 class Manager extends User {
-  constructor(name, password, email) {
+  #isAdm;
+  constructor(name, password, email, isAdm = true) {
     super(name, password, email);
-    database.addManager(this.manageObject)
+    this.#isAdm = isAdm;
+    database.addManager(this.manageObject);
+  }
+
+  get isAdm() {
+    return this.#isAdm;
   }
 
   get manageObject() {
     return {
+      isAdm: this.#isAdm,
       id: super.idUser,
       password: super.password,
       email: super.email,
-      image: 'https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png'
+      image:
+        "https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png",
     };
   }
 
